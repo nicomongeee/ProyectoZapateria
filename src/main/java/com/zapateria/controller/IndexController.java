115f64f3a2f643e0735d37854aeff6c297476443
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2022-11-03 14:18:04 -0600

    Se agregaron las clases de mujeres, hombres y articulos. Ademas que se cambio la ruta de las imagenes. Se agreago imagen al index

--- src/main/java/com/zapateria/controller/IndexController.java
+++ src/main/java/com/zapateria/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.zapateria.controller;
 
 import com.zapateria.services.UsuarioService;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,15 +15,8 @@ public class IndexController {
     private UsuarioService usuarioService;
 
     @GetMapping("/")
-    public String inicio(Model model) {
-        var texto = "Estamos en semana 4";
-        model.addAttribute("saludo", texto);
-
-        /*Usuario usuario1= new Usuario("Pedro", "pjimenez@gmail.com");
-        Usuario usuario2= new Usuario("Juan", "pretana@gmail.com");
-        Usuario usuario3= new Usuario("Nicole", "nmongeg@gmail.com");
+    public String inicio(Model model) {     
         
-        var usuario=Arrays.asList(usuario1, usuario2,usuario3);*/
         var usuarios = usuarioService.getUsuario(); //hace select de la tabla y devuelve un arraylist
 
         model.addAttribute("usuarios", usuarios);
